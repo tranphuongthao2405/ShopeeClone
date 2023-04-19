211b7d54208608c61701b7b621f5274c2af46255
tranphuongthao2405 <tranphuongthao24051999@gmail.com>
2023-04-20 02:32:28 +0700

    init

---
+++ src/useRouteElements.tsx
@@ -0,0 +1,34 @@
+import { useRoutes } from 'react-router-dom';
+import ProductList from './pages/ProductList';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import RegisterLayout from './layout/RegisterLayout';
+
+const useRouteElements = () => {
+  const routeElement = useRoutes([
+    {
+      path: '/',
+      element: <ProductList />,
+    },
+    {
+      path: '/login',
+      element: (
+        <RegisterLayout>
+          <Login />
+        </RegisterLayout>
+      ),
+    },
+    {
+      path: '/register',
+      element: (
+        <RegisterLayout>
+          <Register />
+        </RegisterLayout>
+      ),
+    },
+  ]);
+
+  return routeElement;
+};
+
+export default useRouteElements;
