48fcc54f685b232d2297750599d86c91f9d27212
tranphuongthao2405 <tranphuongthao24051999@gmail.com>
2023-04-29 17:34:22 +0700

    code UI for register layout

--- src/layout/RegisterLayout/RegisterLayout.tsx
+++ src/layout/RegisterLayout/RegisterLayout.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import Footer from 'src/components/Footer';
+import RegisterHeader from 'src/components/RegisterHeader';
 
 interface Props {
   children?: React.ReactNode;
 }
 
 const RegisterLayout = ({ children }: Props) => {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <RegisterHeader />
+      {children}
+      <Footer />
+    </div>
+  );
 };
 
 export default RegisterLayout;
